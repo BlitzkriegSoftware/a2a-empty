2792c47d4d0c99c5b48ec2735c14f21b2db67931
artivsolito <150114098+artivsolito@users.noreply.github.com>
2025-10-29 00:34:00 +0800

    feat: set up uv and install packages, test it is actually working

---
+++ src/good/a2a-server-tests/test_app.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from good.a2a_server.app import app
+
+client = TestClient(app)
+
+def test_hello():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from good a2a_server!"}
